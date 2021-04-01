7472739b4359ae29f1c99e2913dc9686c5106f59
michael <michaelgilch93@gmail.com>
2021-04-01 20:27:48 +0200

    Messy derived template class of cv::Mat_ that can be constructed upon a py::array_t (corresponds to a numpy array) and performs operations on it, as a reference. Upon returning an array_t a new numpy array is constructed as py::array_t doesn't get a base object (for making its lifetime depend on it)

---
+++ src/pybind/matrix-binding.hh
@@ -0,0 +1,80 @@
+#pragma once
+#include <opencv2/core/core.hpp>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+
+static std::vector<int> convert(const std::vector<long>& p_vec)
+{
+    std::vector<int> out;
+    out.reserve(p_vec.size());
+    for (long val : p_vec)
+    {
+        out.push_back(val);
+    }
+    return out;
+}
+
+template<typename T> class CMat : public cv::Mat_<T>
+{
+public:
+    static CMat wrapArray(py::array_t<T> xs)
+    {
+        return CMat<T>(xs);
+    }
+
+    static CMat copyArray(py::array_t<T> xs)
+    {
+        return CMat<T>(py::array_t<T>(xs.request(true)));
+    }
+
+    //This implementation will use the constructor of cv::Mat_(int, int*, T*) for general Nd-arrays
+    CMat(py::array_t<T> xs)
+        : cv::Mat_<T>(int(xs.request(true).shape.size()),
+                      convert(xs.request(true).shape).data(),
+                      static_cast<T*>(xs.request(true).ptr))
+    {
+    }
+
+    CMat() : cv::Mat_<T>() { }
+
+    CMat(const CMat& p_other) : cv::Mat_<T>(p_other) { }
+    CMat(CMat&& p_other) : cv::Mat_<T>(std::forward<CMat&&>(p_other)) { }
+
+    CMat& operator=(const cv::MatExpr& expr)
+    {
+        cv::Mat::operator=(std::forward<const cv::MatExpr&>(expr));
+        return *this;
+    }
+
+    CMat(const cv::MatExpr& e) : cv::Mat(std::forward<const cv::MatExpr&>(e)) { }
+
+    //customMat(const cv::MatExpr& mat) : cv::Mat_<T>(mat) { }
+    CMat(const cv::Mat_<T>& mat) : cv::Mat_<T>(mat) { }
+
+    CMat(int p_row, int p_column) : cv::Mat_<T>(p_row, p_column, 1) { }
+
+    T* data()
+    {
+        return static_cast<T*>(static_cast<void*>(this->ptr()));
+    }
+
+public:
+
+    py::array_t<T> toArray()
+    {
+        return py::array_t<T>(py::buffer_info(this->data(),
+                                              sizeof(T),
+                                              py::format_descriptor<T>::format(),
+                                              2,
+                                              {this->rows, this->cols},
+                                              {sizeof(T) * this->cols, sizeof(T)}));
+    }
+    operator py::array_t<T>()
+    {
+        return toArray();
+    }
+
+};
---
+++ src/pybind/module-init.cc
@@ -0,0 +1,36 @@
+#include "matrix-binding.hh"
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+/**  @brief
+*
+*  This file serves as a central unit for all the python bindings to be done.
+*  Each definition "package" can be forward declared here and added to the PYBIND11_MODULE macro.
+*  This reduces compile time and modularizes the code.
+*
+*  Usage: Forward declare init function. Add it in macro
+*  https://pybind11.readthedocs.io/en/stable/faq.html#how-can-i-reduce-the-build-time
+*
+*/
+
+
+//Example function of a transform
+py::array_t<double> transform(py::array_t<double> p_mat, double p_factor, double p_offset)
+{
+    py::array_t<double> workingCopy{p_mat.request(true)}; //Perform a deep copy
+    CMat<double> mat(workingCopy);
+    mat = mat*p_factor+p_offset;
+    return workingCopy;
+}
+
+
+PYBIND11_MODULE(cvMatBindpy, m)
+{
+    py::class_<CMat<double>>(m, "CMat").def(py::init(&CMat<double>::wrapArray));
+
+    //py::array::forcecast is the default argument and ensures that non conforming arguments are converted into an array
+    py::implicitly_convertible<py::array_t<double>, CMat<double>>();
+
+    m.def("transform", &transform);
+}
