ea998b2aeb85774c0eb1c006445d42385def8940
michael <michaelgilch93@gmail.com>
2021-08-01 20:11:31 +0200

    Fixing bug that converter was specialized only on double type but can easily be written as a template for all types
    Fixing important bug for memory model: Returning an input array, won't lead to segmentation fault anymore because a deep copy is automatically made on opencv::Matrix objects that are non owning (defered by the u-pointer). This ensures that we always return a valid object with its own lifetime memory management.
    
    For the case of a pass through function, that just returns the input numpy array, the disadvantage is obvious: We have to perform a deep copy because we can't reference the original (input) numpy array.

--- src/pybind/matrix-converter.hh
+++ src/pybind/matrix-converter.hh
@@ -4,32 +4,56 @@
 
 namespace py = pybind11;
 
-namespace CMatrixBinding
-{
-/// Returns a numpy array that doesn't perform a deep copy of CMat<T>, but doesn't own the data
-template<typename T> py::array_t<T> ToNumpyArray(const CMat<T>& p_mat);
+namespace CMatrixBinding {
+/// Returns a numpy array that doesn't perform a deep copy of CMat<T>, but
+/// doesn't own the data
+template <typename T> py::array_t<T> ToNumpyArray(const CMat<T> &p_mat);
 
-template<typename T> CMat<T>* ToMat(py::array_t<T> p_arr);
+template <typename T> CMat<T> *ToMat(py::array_t<T> p_arr);
 }; // namespace CMatrixBinding
 
 /**
  *  @brief Pybind11 custom type caster between numpy array and our custom matrix
  *
- *  Handles conversions between numpy arrays and CMat without any user interaction. Completely hides the binding from users on python and C++ side
+ *  Handles conversions between numpy arrays and CMat without any user
+ * interaction. Completely hides the binding from users on python and C++ side
  */
-template<> struct py::detail::type_caster<CMat<double>> : public type_caster_base<CMat<double>>
-{
-    using base = type_caster_base<CMat<double>>;
+template <typename T>
+struct py::detail::type_caster<CMat<T>> : public type_caster_base<CMat<T>> {
+  using base = type_caster_base<CMat<T>>;
 
 public:
-    /// Handle ingoing cast: Python to C++
-    /// We ignore boolean for convert because we always convert and never want to expose cv::Mat in C++ code
-    bool load(py::handle src, bool);
+  /// Handle ingoing cast: Python to C++
+  /// We ignore boolean for convert because we always convert and never want to
+  /// expose cv::Mat in C++ code
+  bool load(py::handle src,
+            bool) // We ignore boolean for convert because we always convert and
+                  // never want to expose cv::Mat in C++ code
+  {
+    if (py::isinstance<py::array_t<T>>(src)) {
+      this->value = CMatrixBinding::ToMat(py::cast<array_t<T>>(src));
+      return true;
+    } else
+      return false;
+  }
 
-    /// Handle outgoing cast: C++ to Python
-    /// Only accepting l-value references to automatically conform to a move return policy
-    static py::handle cast(CMat<double>&& p_mat, py::return_value_policy, py::handle);
+  /// Handle outgoing cast: C++ to Python
+  /// Only accepting l-value references to automatically conform to a move
+  /// return policy
+  static py::handle
+  cast(CMat<T> &&p_mat, py::return_value_policy,
+       py::handle) { // Create the numpy array from custom matrix
+    py::array_t<T> arr = CMatrixBinding::ToNumpyArray(p_mat);
+
+    // Because we want to return ownership to python, we need to manually
+    // increment it's reference count
+    arr.inc_ref();
+
+    return arr;
+  }
 };
 
-//Include the .inl.hpp file here so class definition above serves as a forward declaration. Due to template class we can't work with a .cpp file so we need this inverted structure
+// Include the .inl.hpp file here so class definition above serves as a forward
+// declaration. Due to template class we can't work with a .cpp file so we need
+// this inverted structure
 #include "matrix-converter.inl.hh"
--- src/pybind/matrix-converter.inl.hh
+++ src/pybind/matrix-converter.inl.hh
@@ -1,70 +1,66 @@
-py::handle py::detail::type_caster<CMat<double>>::cast(CMat<double>&& p_mat, py::return_value_policy, py::handle)
-{
-    //Create the numpy array from custom matrix
-    py::array_t<double> arr = CMatrixBinding::ToNumpyArray(p_mat);
 
-    //Because we want to return ownership to python, we need to manually increment it's reference count
-    arr.inc_ref();
+template <typename T>
+py::array_t<T> CMatrixBinding::ToNumpyArray(const CMat<T> &p_mat) {
+  // give memory cleanup responsibility to the Numpy array
 
-    return arr;
+  // Get vector of dimensions
+  std::vector<ssize_t> dims;
+
+  // This behavior rests a design decision. Ultimately we can't mimic the
+  // behavior of of numpy, as the dimensions member is always >= 2, and can't be
+  // one.
+  for (int i = 0; i < p_mat.dims; ++i) {
+    dims.push_back(p_mat.size[i]);
+  }
+
+  // Due to reference semantics of cv::Mat we want to allocate a new array, that
+  // has data implicitely shared. This will increase the ref count by one. We
+  // can't do this with the this pointer, as the current matrix might go out of
+  // scope, invalidating the this pointer and our possibility to call release on
+  // it.
+  CMat<T> *refCountDummy;
+
+  // This is a hack: In order to avoid segmentation faults to return data that
+  // isn't owned by this matrix We check if the u-pointer in the matrix isn't
+  // null. If this is the case we have an owning view! -> We return the complete
+  // array. Otherwise we deep copy.
+  if (p_mat.u) {
+    refCountDummy = new CMat<T>(p_mat);
+  } else {
+    refCountDummy = new CMat<T>(p_mat.clone());
+  }
+
+  // Hand over our ref counting dummy for deletion in a capsule from the python
+  // side
+  py::capsule free_when_done(
+      refCountDummy, [](void *f) { delete reinterpret_cast<CMat<T> *>(f); });
+
+  // Not dealing with strides yet
+  if (p_mat.isContinuous()) {
+
+    // Using a constructor only with dimensions here. "If strides are not
+    // provided, they are deduced" if strides are notprovided, they are deduced
+    // assuming C-contiguity.  Also added simplified constructors for
+    // 1-dimensionalcase." From the official documentation (1.18)
+    // https://pybind11.readthedocs.io/_/downloads/en/latest/pdf/
+    return py::array_t<T>(
+        dims,                                       // shape
+        reinterpret_cast<T *>(refCountDummy->data), // the data pointer
+        free_when_done); // numpy array references this parent
+  } else {
+    throw std::logic_error("Non continuous array. Not implemented yet, need "
+                           "strides!"); // TODOMG:
+                                        // implement
+                                        // with
+                                        // strides
+    return py::array_t<T>();
+  }
 }
 
-bool py::detail::type_caster<CMat<double>>::load(
-    py::handle src,
-    bool) //We ignore boolean for convert because we always convert and never want to expose cv::Mat in C++ code
-{
-    if (py::isinstance<py::array_t<double>>(src))
-    {
-        value = CMatrixBinding::ToMat(py::cast<array_t<double>>(src));
-        return true;
-    }
+template <typename T> CMat<T> *CMatrixBinding::ToMat(py::array_t<T> p_arr) {
+  py::buffer_info info = p_arr.request(true);
 
-    return false;
-}
+  std::vector<int> shapeVector{info.shape.begin(), info.shape.end()};
 
-template<typename T> py::array_t<T> CMatrixBinding::ToNumpyArray(const CMat<T>& p_mat)
-{
-    // give memory cleanup responsibility to the Numpy array
-
-    //Get vector of dimensions
-    std::vector<ssize_t> dims;
-
-    //This behavior rests a design decision. Ultimately we can't mimic the behavior of of numpy, as the dimensions member is always >= 2, and can't be one.
-    for (int i = 0; i < p_mat.dims; ++i)
-    {
-        dims.push_back(p_mat.size[i]);
-    }
-
-    // Due to reference semantics of cv::Mat we want to allocate a new array, that has data implicitely shared.
-    // This will increase the ref count by one. We can't do this with the this pointer, as the current matrix might go out of scope, invalidating the this pointer
-    // and our possibility to call release on it.
-   CMat<T>* refCountDummy = new CMat<T>(p_mat);
-
-    // Hand over our ref counting dummy for deletion in a capsule from the python side
-    py::capsule free_when_done(refCountDummy, [](void* f) { delete reinterpret_cast<CMat<T>*>(f); });
-
-    //Not dealing with strides yet
-    if (p_mat.isContinuous())
-    {
-        //Using a constructor only with dimensions here. "If strides are not provided, they are deduced" if strides are notprovided, they are deduced assuming C-contiguity.  Also added simplified constructors for 1-dimensionalcase."
-        //From the official documentation (1.18) https://pybind11.readthedocs.io/_/downloads/en/latest/pdf/
-        return py::array_t<double>(dims,                                  // shape
-                                   reinterpret_cast<double*>(p_mat.data), // the data pointer
-                                   free_when_done);                       // numpy array references this parent
-    }
-    else
-    {
-        throw std::logic_error(
-            "Non continuous array. Not implemented yet, need strides!"); //TODOMG: implement with strides
-        return py::array_t<T>();
-    }
-}
-
-template<typename T> CMat<T>* CMatrixBinding::ToMat(py::array_t<T> p_arr)
-{
-    py::buffer_info info = p_arr.request(true);
-
-    std::vector<int> shapeVector {info.shape.begin(), info.shape.end()};
-
-    return new CMat<T>(shapeVector, static_cast<T*>(info.ptr));
+  return new CMat<T>(shapeVector, static_cast<T *>(info.ptr));
 }
